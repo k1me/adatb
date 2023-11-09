b58f231fc70bed0199dd8a3d04ec164d7da6c52f
k1me <m.kiraly21@proton.me>
2023-11-09 20:09:11 +0100

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { GuestListComponent } from './guest-list/guest-list.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { RoomTypeComponent } from './room-type/room-type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    GuestListComponent,
+    ReservationComponent,
+    RoomTypeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/room-type/room-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-type',
+  templateUrl: './room-type.component.html',
+  styleUrls: ['./room-type.component.scss']
+})
+export class RoomTypeComponent {
+
+}
